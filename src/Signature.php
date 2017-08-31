780e939173311d18b74007a39eaab0d3e99a666e
Tom Lorentsen <toml@giffgaff.co.uk>
2017-08-31 16:30:58 +0100

    Allow filtering params in older version of PHP

--- src/Signature.php
+++ src/Signature.php
@@ -25,10 +25,7 @@ class Signature
      */
     public function generate($params)
     {
-        if (array_key_exists('merchantSig', $params)) {
-            $params = array_filter($params, function($key) { return 'merchantSig' !== $key; }, ARRAY_FILTER_USE_KEY);
-        }
-        return adyen_hmac($this->hmacKey, $params);
+        return adyen_hmac($this->hmacKey, $this->filterParams($params));
     }
 
     /**
@@ -40,5 +37,27 @@ class Signature
     public function validate($signature, $params)
     {
         return $signature === $this->generate($params);
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return array
+     */
+    private function filterParams($params)
+    {
+        if (!array_key_exists('merchantSig', $params)) {
+            return $params;
+        }
+        if (!defined('ARRAY_FILTER_USE_KEY')) {
+            $filteredParams = array();
+            foreach ($params as $key => $param) {
+                if ('merchantSig' !== $key) {
+                    continue;
+                }
+                $filteredParams[$key] = $param;
+            }
+        }
+        return array_filter($params, function($key) { return 'merchantSig' !== $key; }, ARRAY_FILTER_USE_KEY);
     }
 }
