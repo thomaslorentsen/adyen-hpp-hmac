bb9b9ffd04ff76e7dc4d3d518616345aff8d934b
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-08-31 20:44:05 +0100

    Fix tests

--- tests/Providers/ValidHashesProvider.php
+++ tests/Providers/ValidHashesProvider.php
@@ -8,7 +8,7 @@ class ValidHashesProvider
     {
         return [
            ['Qeft2NQVCBnIYWXOkNT9Lkcs6gV4To6M9wktDPEypX8=', 'F00B44', ['test' => 'test']],
-           ['Qeft2NQVCBnIYWXOkNT9Lkcs6gV4To6M9wktDPEypX8=', 'F00B44', ['test' => 'test', 'blank' => '']],
+           ['F4EeBbhTP8Kc6DxyEK9RfcoHfUMyr5SDlHF+fw2OVwk=', 'F00B44', ['test' => 'test', 'blank' => '']],
         ];
     }
 
@@ -19,7 +19,7 @@ class ValidHashesProvider
                 'test' => 'test',
                 'merchantSig' => 'hello world',
             ]],
-            ['Qeft2NQVCBnIYWXOkNT9Lkcs6gV4To6M9wktDPEypX8=', 'F00B44', [
+            ['F4EeBbhTP8Kc6DxyEK9RfcoHfUMyr5SDlHF+fw2OVwk=', 'F00B44', [
                 'test' => 'test',
                 'merchantSig' => 'hello world',
                 'blank' => '',
