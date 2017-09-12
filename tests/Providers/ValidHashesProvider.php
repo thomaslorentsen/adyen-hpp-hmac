70c7fac5a1b7f211920654e72b2b5a0417dc790b
Tom Lorentsen <toml@giffgaff.co.uk>
2017-09-12 16:30:26 +0100

    Add additional tests for blank and zero

--- tests/Providers/ValidHashesProvider.php
+++ tests/Providers/ValidHashesProvider.php
@@ -9,6 +9,8 @@ class ValidHashesProvider
         return [
            ['Qeft2NQVCBnIYWXOkNT9Lkcs6gV4To6M9wktDPEypX8=', 'F00B44', ['test' => 'test']],
            ['F4EeBbhTP8Kc6DxyEK9RfcoHfUMyr5SDlHF+fw2OVwk=', 'F00B44', ['test' => 'test', 'blank' => '']],
+           ['SWQ/OnS7jnfQAfUPq2YzLQu1H9tUxQ9pLTttjsbq6qQ=', 'F00B44', ['test' => 'test', 'blank' => '', 'payment' => '']],
+           ['7Qpfp8rsP9IQg68xvaAqVwQixXGG+kUhj946ZZvF5JM=', 'F00B44', ['test' => 'test', 'blank' => '', 'payment' => '0']],
         ];
     }
 
